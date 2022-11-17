5b67b0530049d69a4e9772ef0a17fef1c8faf3e5
Daniel Balloch <43333774+danielpbdesign@users.noreply.github.com>
2022-11-18 10:33:12 +1300

    add basic images carousel

--- src/templates/no_data.js
+++ src/templates/no_data.js
@@ -1,19 +1,95 @@
-import React from "react"
+import React, {useState} from "react"
 import { graphql } from "gatsby"
+import styled from '@emotion/styled'
+
+const Wrapper = styled.div`
+max-width: 900px;
+margin: 50px auto;
+.mainImg {
+    border-radius: 10px;
+}
+.buttons {
+    /* position: absolute; */
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-top: -80px;
+    margin-bottom: 80px;
+    button {
+        color: black;
+        background-color: rgba(0,0,0,0);
+        border: none;
+        :hover {
+            cursor:pointer;
+        }
+    }
+    .arrow {
+    height: 16px;
+    width: 16px;
+    border: solid lightgrey;
+    border-width: 0 5px 5px 0;
+    display: inline-block;
+    padding: 3px;
+    transform: rotate(-45deg);
+    -webkit-transform: rotate(-45deg);
+    transition: .3s;
+    :hover {
+        border: solid grey;
+        border-width: 0 5px 5px 0;
+    }
+    }
+    .left {
+    transform: rotate(-225deg);
+    -webkit-transform: rotate(-225deg);
+    }
+    .dot {
+        width: 12px;
+        height: 12px;
+        background-color:lightgrey;
+        border-radius: 30px;
+        margin: 2px 6px;
+        transition: .3s;
+        box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;
+        /* border-radius: 2px; */
+        :hover {
+            background-color:grey;
+            box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px, rgba(10, 37, 64, 0.35) 0px -2px 6px 0px inset;
+            
+        }
+    }
+    .active {
+        background-color: grey;
+        box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
+        
+    }
+}
+`
 
 export default function PropertyPage({data, pageContext}){
+    const [activePicture, setActivePicture] = useState(0)
     console.log(data);
     console.log(pageContext)
+    console.log(data.palacePropertyImages.data[pageContext.slug].length)
+    
     let props = data.palacePropertyDetails.data[pageContext.slug];
+    let imageProps = data.palacePropertyImages.data[pageContext.slug];
+    console.log(imageProps.length)
     return(
-        <div>
-            <img src={data.palacePropertyImages.data[pageContext.slug][0].PropertyImageURL}></img>
+        <Wrapper>
+            <img src={imageProps[activePicture].PropertyImageURL}></img>
+            <div className="buttons">
+                <button onClick={() => {if(activePicture>0){setActivePicture(activePicture-1)}}}><span className="arrow left"/></button>
+                {imageProps.map((X,i) => 
+                    <button onClick={() => {setActivePicture(i)}} className={activePicture == i ? "dot active" : "dot"}/>
+                )}
+                <button onClick={() => {if(activePicture<imageProps.length-1){setActivePicture(activePicture+1)} else {setActivePicture(0)}}}><span className="arrow"/></button>
+            </div>
             <h2>{props.PropertyFeatures.PropertyHeader}</h2>
             <p>Location: {props.PropertyAddress1+" "+props.PropertyAddress2+" "+props.PropertyAddress3+" "+props.PropertyAddress4}</p>
             <p>Rent Amount: ${props.PropertyRentAmount} Per {props.PropertyRentalPeriod}</p>
             <p>Date Available: {props.PropertyDateAvailable}</p>
             <p>{props.PropertyFeatures.PropertyAdvertText}</p>
-        </div>
+        </Wrapper>
     )
 }
 
