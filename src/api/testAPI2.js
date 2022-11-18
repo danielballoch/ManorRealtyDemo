348ab6170f3e00971af86aaf83c0c19b6bacc516
Daniel Balloch <43333774+danielpbdesign@users.noreply.github.com>
2022-11-18 19:47:02 +1300

    update client code

--- src/api/testAPI2.js
+++ src/api/testAPI2.js
@@ -7,33 +7,33 @@ export default async (req, res) => {
     let api_url = "https://www.tpsportal.co.nz/api/v1/tenancy_application/create_property"
     var api_date = new Date().toISOString();
     console.log("reqdata: ",req.body)
-    let testapi_body = JSON.stringify({
-        client_code:"5866",
-        property_code: "ABC123",
-        agent_name: "Bob Testing",
-        agent_email: "bob@testing.com",
-        unit: "1",
-        street_number: "123",
-        street_name: "Testing Avenue",
-        suburb: "Testingburb",
-        city: "Testingville",
-        postcode: "1234"
-    })
-    // let api_body;
-    // if (req.body){
-    //     api_body = JSON.stringify({
-    //         client_code: req.body.client_code,
-    //         property_code: req.body.property_code,
-    //         agent_name: req.body.agent_name,
-    //         agent_email: req.body.agent_email,
-    //         unit: req.body.unit,
-    //         street_number: req.body.number,
-    //         street_name: req.body.street_name,
-    //         suburb: req.body.suburb,
-    //         city: req.body.city,
-    //         postcode: req.body.postcode
-    //     })
-    // }
+    // let testapi_body = JSON.stringify({
+    //     client_code:"5866",
+    //     property_code: "ABC123",
+    //     agent_name: "Bob Testing",
+    //     agent_email: "bob@testing.com",
+    //     unit: "1",
+    //     street_number: "123",
+    //     street_name: "Testing Avenue",
+    //     suburb: "Testingburb",
+    //     city: "Testingville",
+    //     postcode: "1234"
+    // })
+    let api_body;
+    if (req.body){
+        api_body = JSON.stringify({
+            client_code: "8754",
+            property_code: req.body.property_code,
+            agent_name: req.body.agent_name,
+            agent_email: req.body.agent_email,
+            unit: req.body.unit,
+            street_number: req.body.number,
+            street_name: req.body.street_name,
+            suburb: req.body.suburb,
+            city: req.body.city,
+            postcode: req.body.postcode
+        })
+    }
     
 
 function sign(endpoint, key, secret, date, body) {
@@ -46,7 +46,7 @@ function sign(endpoint, key, secret, date, body) {
     .toString('hex');
 }
 
-    var signature = sign(api_url,api_public_key,api_secret,api_date,testapi_body) 
+    var signature = sign(api_url,api_public_key,api_secret,api_date,api_body) 
     const options = {
         method: 'post', 
         headers: {
@@ -56,7 +56,7 @@ function sign(endpoint, key, secret, date, body) {
             'X-API-KEY': api_public_key,
             'X-API-SIGNATURE': signature
         },
-        body: testapi_body
+        body: api_body
     }
     try{
         console.log("are we getting here?")
